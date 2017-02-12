39f90721c6c44b659afbd81406f3c69efc42e05e
ooliynyk <o.p.oliynyk@gmail.com>
2017-02-12 21:28:09 +0200

    Updated doc resources

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -11,7 +11,7 @@ L
 					maxZoom : 18,
 					id : 'mapbox.streets',
 					accessToken : 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw',
-					zIndex: 1
+					zIndex : 1
 				}).addTo(map);
 
 var markersLayer = new L.LayerGroup();
@@ -21,44 +21,45 @@ var geocoder = new L.Control.Geocoder.Nominatim();
 
 // control that shows search box
 var search = L.control();
-search.onAdd = function (map) {
+search.onAdd = function(map) {
 	this._div = L.DomUtil.get('search');
 	return this._div;
 };
 search.addTo(map);
 
 var info = L.control();
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class
-													// "info"
-    this.update();
-    return this._div;
+info.onAdd = function(map) {
+	this._div = L.DomUtil.create('div', 'info'); // create a div with a class
+	// "info"
+	this.update();
+	return this._div;
 };
 // method that we will use to update the control based on feature properties
 // passed
-info.update = function (props) {
-    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
-        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-        : 'Hover over a state');
+info.update = function(props) {
+	this._div.innerHTML = '<h4>Country cooperation value</h4>'
+			+ (props ? '<b>' + props.ADMIN + '</b><br />' + props.density
+					: 'Hover over a country');
 };
 info.addTo(map);
 
-var legend = L.control({position: 'bottomright'});
-legend.onAdd = function (map) {
+var legend = L.control({
+	position : 'bottomright'
+});
+legend.onAdd = function(map) {
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [];
+	var div = L.DomUtil.create('div', 'info legend'), grades = [ 0, 10, 20, 50,
+			100, 200, 500, 1000 ], labels = [];
 
-    // loop through our density intervals and generate a label with a colored
+	// loop through our density intervals and generate a label with a colored
 	// square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
+	for (var i = 0; i < grades.length; i++) {
+		div.innerHTML += '<i style="background:' + getColor(grades[i] + 1)
+				+ '"></i> ' + grades[i]
+				+ (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	}
 
-    return div;
+	return div;
 };
 legend.addTo(map);
 
@@ -66,86 +67,81 @@ var geojson = L.geoJson(countries);
 
 // get color depending on population density value
 function getColor(d) {
-	return d > 1000 ? '#800026' :
-			d > 500  ? '#BD0026' :
-			d > 200  ? '#E31A1C' :
-			d > 100  ? '#FC4E2A' :
-			d > 50   ? '#FD8D3C' :
-			d > 20   ? '#FEB24C' :
-			d > 10   ? '#FED976' :
-						'#FFEDA0';
+	return d > 1000 ? '#800026' : d > 500 ? '#BD0026' : d > 200 ? '#E31A1C'
+			: d > 100 ? '#FC4E2A' : d > 50 ? '#FD8D3C' : d > 20 ? '#FEB24C'
+					: d > 10 ? '#FED976' : '#FFEDA0';
 }
 
 function style(feature) {
 	return {
-		weight: 2,
-		opacity: 1,
-		color: 'white',
-		dashArray: '3',
-		fillOpacity: 0.7,
-		fillColor: getColor(feature.properties.density)
+		weight : 2,
+		opacity : 1,
+		color : 'white',
+		dashArray : '3',
+		fillOpacity : 0.7,
+		fillColor : getColor(feature.properties.density)
 	};
 }
 
 function highlightFeature(e) {
-    var layer = e.target;
+	var layer = e.target;
 
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
+	layer.setStyle({
+		weight : 5,
+		color : '#666',
+		dashArray : '',
+		fillOpacity : 0.7
+	});
 
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
-    
+	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+		layer.bringToFront();
+	}
+
 	info.update(layer.feature.properties);
 }
 
 function resetHighlight(e) {
-    geojson.resetStyle(e.target);
+	geojson.resetStyle(e.target);
 	info.update();
 }
 
 function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
+	map.fitBounds(e.target.getBounds());
 }
 
 function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
+	layer.on({
+		mouseover : highlightFeature,
+		mouseout : resetHighlight,
+		click : zoomToFeature
+	});
 }
 
 function drawColorfulCountries(geoNetwork) {
 	// console.log(geoNetwork);
 	// console.log(geojson);
-	
-	geojson = L.geoJson(countries, {
-	    filter: function(feature, layer) {
-	    	var country = feature.properties.ADMIN;
-	    	console.log(country);
 
-	    	for (let geoNode of geoNetwork) {
-	    		if (geoNode.country == country)
-	    			return true;
-	    	}
-	    	return false;
-	    },
-		style: style,
-	    onEachFeature: onEachFeature
+	geojson = L.geoJson(countries, {
+		filter : function(feature, layer) {
+			var country = feature.properties.ADMIN;
+			console.log(country);
+
+			if (geoNetwork.has(country)) {
+				feature.properties.density = geoNetwork.get(country);
+				return true;
+			}
+			return false;
+		},
+		style : style,
+		onEachFeature : onEachFeature
 	}).addTo(map);
 }
 
 function drawNode(label, size, location) {
 	var marker = new L.CircleMarker(location, {
 		radius : size,
-		riseOnHover: true,
-		fillOpacity: 1
+		riseOnHover : true,
+		fillOpacity : 1
 	});
 	marker.bindTooltip(label, {
 		permanent : false,
@@ -169,8 +165,8 @@ function networkLayer(network) {
 
 	var nodeSizeCoef = calculateNodeSizeCoef(network.nodes);
 
-	var geoNetwork = new Set();
-	
+	var geoNetwork = new Map();
+
 	var processingCounter = 0;
 
 	network.nodes.forEach(function(node) {
@@ -178,28 +174,27 @@ function networkLayer(network) {
 
 		geocoder.geocode(node.label, function(results) {
 			processingCounter++;
-			
+
 			if (results == null || results.length == 0)
 				return;
-			
+
 			var latLng = new L.LatLng(results[0].center.lat,
 					results[0].center.lng);
 
 			var country = results[0].properties.address.country;
 
-			geoNetwork.add({
-				id : node.id,
-				value : node.value,
-				country : country
-			});
+			var value = node.value;
+			if (geoNetwork.has(country)) {
+				value += geoNetwork.get(country);
+			}
+			geoNetwork.set(country, value);
 
 			var nodeSize = node.value * nodeSizeCoef;
 			drawNode(node.label, Math.max(nodeSize, 5), latLng);
-			
+
 			if (processingCounter == network.nodes.length) {
 				drawColorfulCountries(geoNetwork);
 			}
 		});
 	});
 }
-
