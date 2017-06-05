7049cfa48810e511d7a3d83f723464835f068667
ooliynyk <o.p.oliynyk@gmail.com>
2017-06-05 10:49:09 +0300

    Changed legend size

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,13 +1,13 @@
 var NODE_SIZE_MAX = 15;
 
-var DUMMY_LOCATION = [ 0, 0 ];
+var DUMMY_LOCATION = [0, 0];
 
-var map = L.map('map').setView([ 51.505, -0.09 ], 3);
+var map = L.map('map').setView([51.505, -0.09], 3);
 
 L
-		.tileLayer(
-				'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
-				{}).addTo(map);
+    .tileLayer(
+        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+        {}).addTo(map);
 
 var markersLayer = new L.LayerGroup();
 markersLayer.addTo(map);
@@ -19,59 +19,59 @@ var geocoder = new L.Control.Geocoder.Nominatim();
 
 // control that shows search box
 var search = L.control();
-search.onAdd = function(map) {
-	this._div = L.DomUtil.get('search');
-	return this._div;
+search.onAdd = function (map) {
+    this._div = L.DomUtil.get('search');
+    return this._div;
 };
 search.addTo(map);
 
 var info = L.control();
-info.onAdd = function(map) {
-	this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-	this.update();
-	return this._div;
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
 };
 // method that we will use to update the control based on feature properties
 // passed
-info.update = function(props) {
-	if (props) {
-		this._div.innerHTML = '<h4>Country cooperation value</h4>' + '<b>'
-				+ props.ADMIN + '</b><br />' + props.density;
-		L.DomUtil.removeClass(this._div, 'invisible');
-	} else {
-		L.DomUtil.addClass(this._div, 'invisible');
-	}
+info.update = function (props) {
+    if (props) {
+        this._div.innerHTML = '<h4>Country cooperation value</h4>' + '<b>'
+            + props.ADMIN + '</b><br />' + props.density;
+        L.DomUtil.removeClass(this._div, 'invisible');
+    } else {
+        L.DomUtil.addClass(this._div, 'invisible');
+    }
 };
 info.addTo(map);
 
 var legend = L.control({
-	position : 'bottomright'
+    position: 'bottomright'
 });
-legend.onAdd = function(map) {
+legend.onAdd = function (map) {
 
-	var div = L.DomUtil.create('div', 'info legend'), grades = [ 0, 10, 20, 50,
-			100, 200, 500, 1000 ], labels = [];
+    var div = L.DomUtil.create('div', 'info legend'), grades = [0, 10, 20, 50,
+        100, 200, 500, 1000], labels = [];
 
-	// loop through our density intervals and generate a label with a colored
-	// square for each interval
-	for (var i = 0; i < grades.length; i++) {
-		div.innerHTML += '<i style="background:' + getColor(grades[i] + 1)
-				+ '"></i> ' + grades[i]
-				+ (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-	}
+    // loop through our density intervals and generate a label with a colored
+    // square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML += '<i style="background:' + getColor(grades[i] + 1)
+            + '"></i> ' + grades[i]
+            + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
 
-	return div;
+    return div;
 };
 legend.addTo(map);
 
 var geojson = L.geoJson(countries);
 
 var dataTableWindow = L.control.window(map, {
-	content : '<table id="coopTable" class="display" width="100%"></table>'
+    content: '<table id="coopTable" class="display" width="100%"></table>'
 });
 
-var dataTableButton = L.easyButton('fa-table', function(btn, map) {
-	makeDataTable();
+var dataTableButton = L.easyButton('fa-table', function (btn, map) {
+    makeDataTable();
 }).addTo(map);
 dataTableButton.disable();
 
@@ -81,6 +81,7 @@ function controlEnter(e) {
     map.dragging.disable();
     map.doubleClickZoom.disable();
 }
+
 function controlLeave() {
     map.dragging.enable();
     map.doubleClickZoom.enable();
@@ -94,176 +95,176 @@ for (var i = 0; i < inputTags.length; i++) {
 
 // get color depending on population density value
 function getColor(d) {
-	return d > 1000 ? '#800026' : d > 500 ? '#BD0026' : d > 200 ? '#E31A1C'
-			: d > 100 ? '#FC4E2A' : d > 50 ? '#FD8D3C' : d > 20 ? '#FEB24C'
-					: d > 10 ? '#FED976' : '#FFEDA0';
+    return d > 1000 ? '#800026' : d > 500 ? '#BD0026' : d > 200 ? '#E31A1C'
+        : d > 100 ? '#FC4E2A' : d > 50 ? '#FD8D3C' : d > 20 ? '#FEB24C'
+            : d > 10 ? '#FED976' : '#FFEDA0';
 }
 
 function style(feature) {
-	return {
-		weight : 2,
-		opacity : 1,
-		color : 'white',
-		dashArray : '3',
-		fillOpacity : 0.7,
-		fillColor : getColor(feature.properties.density)
-	};
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.density)
+    };
 }
 
 function highlightFeature(e) {
-	var layer = e.target;
+    var layer = e.target;
 
-	layer.setStyle({
-		weight : 5,
-		color : '#666',
-		dashArray : '',
-		fillOpacity : 0.7
-	});
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
 
-	info.update(layer.feature.properties);
+    info.update(layer.feature.properties);
 }
 
 function resetHighlight(e) {
-	geojson.resetStyle(e.target);
-	info.update();
+    geojson.resetStyle(e.target);
+    info.update();
 }
 
 function zoomToFeature(e) {
-	map.fitBounds(e.target.getBounds());
+    map.fitBounds(e.target.getBounds());
 }
 
 function onEachFeature(feature, layer) {
-	layer.on({
-		mouseover : highlightFeature,
-		mouseout : resetHighlight,
-		click : zoomToFeature
-	});
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
 }
 
 function drawColorfulCountries(geoNetwork) {
-	geojson = L.geoJson(countries, {
-		filter : function(feature, layer) {
-			var country = feature.properties.ADMIN;
-			console.log(country);
+    geojson = L.geoJson(countries, {
+        filter: function (feature, layer) {
+            var country = feature.properties.ADMIN;
+            console.log(country);
 
-			if (geoNetwork.has(country)) {
-				feature.properties.density = geoNetwork.get(country);
-				return true;
-			}
-			return false;
-		},
-		style : style,
-		onEachFeature : onEachFeature
-	}).addTo(geojsonLayer);
+            if (geoNetwork.has(country)) {
+                feature.properties.density = geoNetwork.get(country);
+                return true;
+            }
+            return false;
+        },
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(geojsonLayer);
 }
 
 function drawNode(label, size, location) {
-	var marker = new L.CircleMarker(location, {
-		radius : size,
-		riseOnHover : true,
-		fillOpacity : 0.85,
-	});
-	marker.bindTooltip(label, {
-		permanent : false,
-		offset : [ 0, 0 ]
-	});
-	// marker.addTo(map);
-	markersLayer.addLayer(marker);
+    var marker = new L.CircleMarker(location, {
+        radius: size,
+        riseOnHover: true,
+        fillOpacity: 0.85,
+    });
+    marker.bindTooltip(label, {
+        permanent: false,
+        offset: [0, 0]
+    });
+    // marker.addTo(map);
+    markersLayer.addLayer(marker);
 }
 
 function calculateNodeSizeCoef(nodes) {
-	var nodeValueMax = 0;
-	for (var i = 0; i < nodes.length; i++) {
-		if (nodes[i].value > nodeValueMax)
-			nodeValueMax = nodes[i].value;
-	}
-	return NODE_SIZE_MAX / nodeValueMax;
+    var nodeValueMax = 0;
+    for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].value > nodeValueMax)
+            nodeValueMax = nodes[i].value;
+    }
+    return NODE_SIZE_MAX / nodeValueMax;
 }
 
 function clearNetwork() {
-	markersLayer.clearLayers();
-	geojsonLayer.clearLayers();
+    markersLayer.clearLayers();
+    geojsonLayer.clearLayers();
 }
 
 var NETWORK = {
-	nodes : []
+    nodes: []
 };
 
 function drawNetwork(network) {
-	clearNetwork();
-	NETWORK = network;
-	dataTableButton.enable();
+    clearNetwork();
+    NETWORK = network;
+    dataTableButton.enable();
 
-	var nodeSizeCoef = calculateNodeSizeCoef(network.nodes);
+    var nodeSizeCoef = calculateNodeSizeCoef(network.nodes);
 
-	var geoNetwork = new Map();
+    var geoNetwork = new Map();
 
-	var processingCounter = 0;
+    var processingCounter = 0;
 
-	var lazyCalls = [];
+    var lazyCalls = [];
 
-	network.nodes.forEach(function(node) {
-		// console.log(node.label);
+    network.nodes.forEach(function (node) {
+        // console.log(node.label);
 
-		geocoder.geocode(node.label, function(results) {
-			processingCounter++;
+        geocoder.geocode(node.label, function (results) {
+            processingCounter++;
 
-			if (results == null || results.length == 0)
-				return;
+            if (results == null || results.length == 0)
+                return;
 
-			var latLng = new L.LatLng(results[0].center.lat,
-					results[0].center.lng);
+            var latLng = new L.LatLng(results[0].center.lat,
+                results[0].center.lng);
 
-			var country = results[0].properties.address.country;
+            var country = results[0].properties.address.country;
 
-			var value = node.value;
-			if (geoNetwork.has(country)) {
-				value += geoNetwork.get(country);
-			}
-			geoNetwork.set(country, value);
+            var value = node.value;
+            if (geoNetwork.has(country)) {
+                value += geoNetwork.get(country);
+            }
+            geoNetwork.set(country, value);
 
-			var nodeSize = node.value * nodeSizeCoef;
+            var nodeSize = node.value * nodeSizeCoef;
 
-			lazyCalls.push(function() {
-				drawNode(node.label, Math.max(nodeSize, 5), latLng);
-			});
+            lazyCalls.push(function () {
+                drawNode(node.label, Math.max(nodeSize, 5), latLng);
+            });
 
-			if (processingCounter == network.nodes.length) {
-				drawColorfulCountries(geoNetwork);
+            if (processingCounter == network.nodes.length) {
+                drawColorfulCountries(geoNetwork);
 
-				lazyCalls.forEach(function(call) {
-					call();
-				});
-			}
-		});
-	});
+                lazyCalls.forEach(function (call) {
+                    call();
+                });
+            }
+        });
+    });
 }
 
 function toggleSpin(enabled) {
-	map.spin(enabled);
-	if (enabled) {
-		dataTableButton.disable();
-	}
+    map.spin(enabled);
+    if (enabled) {
+        dataTableButton.disable();
+    }
 }
 
 function makeDataTable() {
-	var dataSet = [];
+    var dataSet = [];
 
-	NETWORK.nodes.forEach(function(node) {
-		dataSet.push([ node.label, node.value ]);
-	});
+    NETWORK.nodes.forEach(function (node) {
+        dataSet.push([node.label, node.value]);
+    });
 
-	$('#coopTable').DataTable({
-		destroy : true,
-		dom : 'Bfrtip',
-		data : dataSet,
-		columns : [ {
-			title : "Organization"
-		}, {
-			title : "Cooperation value"
-		} ],
-		buttons : [ 'copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5' ]
-	});
+    $('#coopTable').DataTable({
+        destroy: true,
+        dom: 'Bfrtip',
+        data: dataSet,
+        columns: [{
+            title: "Organization"
+        }, {
+            title: "Cooperation value"
+        }],
+        buttons: ['copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5']
+    });
 
-	dataTableWindow.show();
+    dataTableWindow.show();
 }
